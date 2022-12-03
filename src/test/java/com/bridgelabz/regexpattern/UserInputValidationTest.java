c5d9686fabb7c721005ec175892b1882ac56c6ec
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2022-12-02 22:12:43 -0800

    Add:Adding program for first name pattern Test Case

---
+++ src/test/java/com/bridgelabz/regexpattern/UserInputValidationTest.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.regexpattern;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserInputValidationTest {
+	@Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserInputValidationTest userInputValidationTest = new UserInputValidationTest();
+        boolean result = userInputValidationTest.isValidFirstName("Dipali");
+        Assert.assertEquals(true, result);
+    }
+    private boolean isValidFirstName(String string) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Test
+    public void givenFirstName_WhenShort_ShouldReturnFalse() {
+        UserInputValidationTest userInputValidationTest = new UserInputValidationTest();
+        boolean result = userInputValidationTest.isValidFirstName("Pr");
+        Assert.assertEquals(false, result);
+    }
+}
