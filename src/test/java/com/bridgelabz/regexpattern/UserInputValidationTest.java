e8b66cca042ac007b72a6181261dd6be2df6d93a
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2022-12-02 23:39:26 -0800

    Adding program for Password Minimum one Numeric

--- src/test/java/com/bridgelabz/regexpattern/UserInputValidationTest.java
+++ src/test/java/com/bridgelabz/regexpattern/UserInputValidationTest.java
@@ -6,19 +6,19 @@ public class UserInputValidationTest {
 	@Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserInputValidation userInputValidation = new UserInputValidation();
-        boolean result = userInputValidation.isValidFirstName("Dipali");
+        boolean result = userInputValidation.isValidFirstName("Dipak");
         Assert.assertEquals(true, result);
     }
     @Test
     public void givenFirstName_WhenShort_ShouldReturnFalse() {
         UserInputValidation userInputValidation = new UserInputValidation();
-        boolean result = userInputValidation.isValidFirstName("Pr");
+        boolean result = userInputValidation.isValidFirstName("dr");
         Assert.assertEquals(false, result);
     }
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue() {
         UserInputValidation userInputValidation = new UserInputValidation();
-        boolean result = userInputValidation.isValidLastName("Sonawane");
+        boolean result = userInputValidation.isValidLastName("bhuwan");
         Assert.assertEquals(true, result);
     }
 
@@ -31,7 +31,7 @@ public class UserInputValidationTest {
     @Test
     public void givenEmail_WhenValid_ShouldReturnTrue() {
         UserInputValidation userInputValidation = new UserInputValidation();
-        boolean result = userInputValidation.isValidEmail("aryabhuwan730@gmail.com");
+        boolean result = userInputValidation.isValidEmail("kunalsonawane730@gmail.com");
         Assert.assertEquals(true, result);
     }
 
